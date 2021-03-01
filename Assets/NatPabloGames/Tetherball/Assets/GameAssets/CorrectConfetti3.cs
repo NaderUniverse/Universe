2495d90005bdb4aff29f9015b48573ee9717e24d
Natalia Colmenares <38693615+nataliacolm@users.noreply.github.com>
2021-02-28 23:19:44 -0500

    adds tetherball game

---
+++ Assets/NatPabloGames/Tetherball/Assets/GameAssets/CorrectConfetti3.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorrectConfetti3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        if(Question2Info.correctConfetti == 0)
+        {
+            gameObject.GetComponent<ParticleSystem>().Stop();
+        }
+        else if(Question2Info.correctConfetti == 1)
+        {
+            gameObject.GetComponent<ParticleSystem>().Play();
+        }
+    }
+}
---
+++ Assets/NatPabloGames/Tetherball/Assets/MenuAssets/ScriptsMenu/SetVolumeTether.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SetVolumeTether : MonoBehaviour
+{
+  public AudioMixer mixer;
+
+  public void SetLevel (float sliderVal)
+  {
+    mixer.SetFloat("MusicVol", Mathf.Log10(sliderVal) * 20);
+  }
+}
