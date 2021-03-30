45bfe620bd9c89ccd7496c48e237eef0cfe8cfe9
Hunter Leboeuf <unconfigured@null.spigotmc.org>
2021-03-30 01:10:37 -0400

    Finsihed coding up all parts to question 11 for pulley game just need to finish the UI for everything but base coding is done for the question

--- Assets/Games/HunterWillGames/DoublePulleyGame/Scripts/PulleyQuestionManager.cs
+++ Assets/Games/HunterWillGames/DoublePulleyGame/Scripts/PulleyQuestionManager.cs
@@ -18,11 +18,11 @@ public class PulleyQuestionManager : MonoBehaviour
     //Int
     public int a, b;//A needs to be between 2-4 and B needs to be inbetween 5-7
     public int Score = 0;
-
+    public int questionPart = 1;
     //float
     //distance is between 0.25m - 2.0m for part 2)xA,xB is distance traveled 
     public float t, distance, acceleration, xB,xA;//t is inbetween 1.0 - 3.0, for part 3) needs to be inbetween 2.0 - 3.0 could use two variables for this
-    private float tmp, VB, VA;
+    public float tmp, VB, VA, DTA;//DTA stands for distance traveled for A
     //Text/strings
     public Text input, output;
     public string question;
@@ -42,12 +42,17 @@ public class PulleyQuestionManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Math();
         // if enter button is pressed enter question
         if (Input.GetButtonDown("Submit"))
         {
             EnterAnswer();
         }
-        Math();
+        if(questionPart == 3)
+        {
+            //if we are on the third question part we change the time between 2 to 3 seconds
+            t = Random.Range(2, 3);
+        }
     }
     void Math()
     {
@@ -55,13 +60,14 @@ public class PulleyQuestionManager : MonoBehaviour
         tmp = (2 * xB) / t;
         VB = tmp;
         VA = -VB / 2;
-        //Debug.Log("answer" + VB+ "VA == " + VA );
+        
         //Second question
         tmp = (2 * xB) / (t * t);
         acceleration = tmp;
-        //Debug.Log(" acceleration" + acceleration);
-        //Third question
 
+        //Third question
+        
+        DTA = (float)(0.5 * acceleration * (t * t));
     }
     void DisplayQuestion()
     {
@@ -69,13 +75,43 @@ public class PulleyQuestionManager : MonoBehaviour
     }
     void EnterAnswer()
     {
-        VB.ToString(answer);
-       
-        Debug.Log(VB);
-        Debug.Log(uInput.text);
-        if (uInput.text == answer)
+        switch(questionPart)
         {
-            Debug.Log("Congrats :)");
+            case 1:
+                  if (uInput.text.Equals(VB.ToString()))
+                  {
+                  Debug.Log("Congrats :)");
+                    questionPart++;
+                  }
+                  else
+                  {
+                    Debug.Log("sorry try again");
+                  }
+               break;
+            case 2:
+                if (uInput.text.Equals(acceleration.ToString()))
+                {
+                    Debug.Log("Congrats :), you have completed 2/3 parts for this question");
+                    questionPart++;
+                }
+                else
+                {
+                    Debug.Log("sorry try again");
+                }
+                break;
+            case 3:
+                if (uInput.text.Equals(DTA.ToString()))
+                {
+                    Debug.Log("Congrats :), you have gotten all of the parts to this question correct");
+                }
+                else
+                {
+
+                    Debug.Log("sorry try again");
+                }
+                break;
+
+
         }
                 
     }
