9162f38e81f38784773aa7fdfffc21cfd7594f00
Exdol <exdoldavy@Knights.ucf.edu>
2021-03-30 20:03:11 -0400

    Added Monkey Vine game to Universe

---
+++ Assets/Games/ExdolJackGames/MonkeyVineGame/Scripts/RightVineRotate.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightVineRotate : MonoBehaviour
+{
+   public GameObject target;
+    public float speed;
+    public SpriteRenderer monkey;
+    public SpriteRenderer hangingMonkey;
+    private Vector3 zAxis = new Vector3(0, 0, 1);
+    private bool canSpin;
+    public SpriteRenderer thisVine;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        canSpin = true;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (canSpin)
+        {
+            transform.RotateAround(target.transform.position, zAxis, speed);
+            Debug.Log(transform.rotation.z);
+            if (transform.rotation.z < -0.3)
+            {
+                speed = -1*speed;
+                monkey.enabled = false;
+                hangingMonkey.enabled = true;
+            }
+            else if(transform.rotation.z > 0.25 && hangingMonkey.enabled == true)
+            {
+                canSpin = false;
+                thisVine.enabled = false;
+            }
+
+        }
+        
+        
+    }
+}
---
+++ Assets/Games/ExdolJackGames/MonkeyVineGame/Scripts/VinePhysics.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VinePhysics : MonoBehaviour
+{
+    public Transform Center;
+    public GameObject leftVine;
+    public GameObject rightVine;
+    public float speed;
+    public HingeJoint2D leftMonkey;
+    public HingeJoint2D rightMonkey;
+    public bool canSpin;
+    private Vector3 zAxis = new Vector3(0, 0, 1);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canSpin = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        if (canSpin)
+        {
+            //Debug.Log("Spinning commence!");
+            transform.RotateAround(leftVine.transform.Position, Vector3.up, 20*Time.deltaTime);
+        }
+        */
+        
+    }
+}
