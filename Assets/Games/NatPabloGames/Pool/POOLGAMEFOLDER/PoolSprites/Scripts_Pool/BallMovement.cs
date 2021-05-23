e466633c4e2e2d5f30f6efc2d66da7b0e6707caf
Natalia Colmenares <38693615+nataliacolm@users.noreply.github.com>
2021-05-23 14:26:39 -0400

    fixes pool game transfer errors

--- Assets/Games/NatPabloGames/Pool/POOLGAMEFOLDER/PoolSprites/Scripts_Pool/BallMovement.cs
+++ Assets/Games/NatPabloGames/Pool/POOLGAMEFOLDER/PoolSprites/Scripts_Pool/BallMovement.cs
@@ -13,9 +13,7 @@ public class BallMovement : MonoBehaviour
   public SpriteRenderer eball;
   public SpriteRenderer cball;
 
-  public bool collisionFlag = false;
-
-  Vector3 originalPos;
+  Vector3 originalPos2;
   Vector3 originalPos1;
 
   private float forceConstant = -5000;
@@ -50,12 +48,6 @@ public class BallMovement : MonoBehaviour
       _anim.SetTrigger("StartFade");
       yield return new WaitForSeconds(.9f);
       eball.enabled = false;
-      //eball.enabled = false;
-  }
-
-  public void changeCollisionFlag()
-  {
-      collisionFlag = !collisionFlag;
   }
 
   public void setVelocities()
@@ -66,10 +58,10 @@ public class BallMovement : MonoBehaviour
 
   void Start()
   {
+    // If the balls are to be restarted in the same position, save the position at the start
     originalPos1 = new Vector3(cueBall.transform.position.x, cueBall.transform.position.y,
                               cueBall.transform.position.z);
-    //cueBall.transform.position;
-    originalPos = new Vector3(eightBall.transform.position.x,eightBall.transform.position.y,
+    originalPos2 = new Vector3(eightBall.transform.position.x,eightBall.transform.position.y,
                               eightBall.transform.position.z);
   }
 
@@ -81,7 +73,8 @@ public class BallMovement : MonoBehaviour
       eightBall.velocity = Vector2.zero;
       cueBall.angularVelocity = 0f;
       eightBall.angularVelocity = 0f;
-      //cueBall.transform.position = originalPos1;
+
+      // Feel free to edit the next position of the balls when the question is restarted
       cueBall.transform.position = new Vector3(-22f, 37f, -4.26f);
       eightBall.transform.position = new Vector3(-5.9f, 21.8f, -4.26f);;
 
@@ -90,6 +83,6 @@ public class BallMovement : MonoBehaviour
   public void moveCue()
   {
     if (!cueFlag)
-      cue.transform.position = new Vector3(originalPos.x, originalPos.y + -Mathf.PingPong(Time.time * 100, 50), originalPos.z);
+      cue.transform.position = new Vector3(originalPos2.x, originalPos2.y + -Mathf.PingPong(Time.time * 100, 50), originalPos2.z);
   }
 }
